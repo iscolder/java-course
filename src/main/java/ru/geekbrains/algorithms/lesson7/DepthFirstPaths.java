ab8859b56608abf2b477f2435c7fd68af3fa4351
kemelyanov <kemelyanov@evelopers.com>
2020-12-18 01:36:19 +0300

    hw7

--- src/main/java/ru/geekbrains/algorithms/lesson7/DepthFirstPaths.java
+++ src/main/java/ru/geekbrains/algorithms/lesson7/DepthFirstPaths.java
@@ -1,16 +1,9 @@
 package ru.geekbrains.algorithms.lesson7;
 
-import java.util.LinkedList;
-
-public class DepthFirstPaths {
-	private final boolean[] marked;
-	private final int[] parents;
-	private final int source;
+public class DepthFirstPaths extends AbstractFirstPaths {
 
 	public DepthFirstPaths(Graph g, int source) {
-		this.source = source;
-		marked = new boolean[g.getVertexCount()];
-		parents = new int[g.getVertexCount()];
+		super(g, source);
 		dfs(g, source);
 	}
 
@@ -18,22 +11,9 @@ public class DepthFirstPaths {
 		marked[v] = true;
 		for (int w : g.getAdjList(v)) {
 			if (!marked[w]) {
-				parents[w] = v;
+				edgeTo[w] = v;
 				dfs(g, w);
 			}
 		}
-	}
-
-	public LinkedList<Integer> pathTo(int dist) {
-		if (!marked[dist]) {
-			return null;
-		}
-		LinkedList<Integer> stack = new LinkedList<>();
-		int current = source;
-		while (current != dist) {
-			stack.push(current);
-			current = parents[current];
-		}
-		return stack;
 	}
 }
--- src/main/java/ru/geekbrains/algorithms/lesson7/Main.java
+++ src/main/java/ru/geekbrains/algorithms/lesson7/Main.java
@@ -2,13 +2,18 @@ package ru.geekbrains.algorithms.lesson7;
 
 public class Main {
 
-
 	public static void main(String[] args) {
-		Graph graph = new Graph(5);
+		Graph graph = new Graph(10);
 		graph.addEdge(1, 2);
 		graph.addEdge(0, 4);
 		graph.addEdge(1, 4);
-
-		System.out.println(graph.getAdjList(1));
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 9);
+		graph.addEdge(8, 4);
+		graph.addEdge(7, 1);
+		graph.addEdge(5, 3);
+		graph.addEdge(6, 0);
+		BreadthFirstPaths breadthFirstPaths = new BreadthFirstPaths(graph, 1);
+		System.out.println(breadthFirstPaths.pathTo(6));
 	}
 }
